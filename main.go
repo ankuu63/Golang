18008e46a6d4fd87b35c389c59b0e84004715b4c
ankuu63 <rajayush4404@gmail.com>
2024-11-16 21:14:47 +0530

    channels in goroutine

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	fmt.Println("hey channels ")
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	mych := make(chan int, 4)
+
+	go func(wg *sync.WaitGroup, ch chan int) {
+
+		fmt.Println(<-mych)
+
+		//fmt.Println(<-mych)
+		wg.Done()
+	}(wg, mych)
+
+	go func(wg *sync.WaitGroup, ch chan int) {
+		mych <- 5
+		mych <- 6
+
+		wg.Done()
+	}(wg, mych)
+
+	wg.Wait()
+}
